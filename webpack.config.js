0711c7f6b8a5297404de4c9ae03dabbfbe59cbfa
remiscarlet <remilia@scarl.et>
2021-05-02 03:48:32 +0000

    Skeleton yarn/webpack project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env, opts) => {
+  return {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '...'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Remilia is best 2hu"
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: 'content/images/', to: 'assets/images/' },
+        ],
+      }),
+    ],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+  }
+};
