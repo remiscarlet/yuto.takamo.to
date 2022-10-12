2fbed76e326a6c539ba2bfa4a3a4445ffe24ab98
remiscarlet <remilia@scarl.et>
2022-10-12 16:33:16 +0000

    Add explicit home link on navbar

--- src/Components/Navbar.vue
+++ src/Components/Navbar.vue
@@ -1,22 +1,49 @@
 <template>
   <div>
-    <b-navbar fixed="top" v-bind:class="{'scrolled-up': isScrollingUp, 'scrolled-down': isScrollingDown}" toggleable="lg" type="dark" variant="alert">
-      <b-navbar-brand href="#" class="text-decoration-none mr-5" to="/">Yuto Takamoto</b-navbar-brand>
+    <b-navbar
+      fixed="top"
+      v-bind:class="{
+        'scrolled-up': isScrollingUp,
+        'scrolled-down': isScrollingDown,
+      }"
+      toggleable="lg"
+      type="dark"
+      variant="alert"
+    >
+      <b-navbar-brand href="#" class="text-decoration-none mr-5" to="/"
+        >Yuto Takamoto</b-navbar-brand
+      >
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" align="right" is-nav>
         <b-navbar-nav align="right" class="ml-auto">
+          <b-nav-item to="">Home</b-nav-item>
           <b-nav-item to="tech">My Tech</b-nav-item>
           <b-nav-item to="projects">Some of My Projects</b-nav-item>
-          <b-nav-item href="../../assets/pdf/yuto.takamoto.resume.2021.06.22.pdf" target="_blank">Resume</b-nav-item>
-          <b-nav-item href="https://www.linkedin.com/in/ytakamoto/" target="_blank">Linkedin</b-nav-item>
+          <b-nav-item
+            href="../../assets/pdf/yuto.takamoto.resume.2021.06.22.pdf"
+            target="_blank"
+            >Resume</b-nav-item
+          >
+          <b-nav-item
+            href="https://www.linkedin.com/in/ytakamoto/"
+            target="_blank"
+            >Linkedin</b-nav-item
+          >
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
-import { BNavbar, BNavbarBrand, BNavbarToggle, BCollapse, BNavbarNav, BNavItem } from 'bootstrap-vue';
+import { Component, Vue, Prop } from "vue-property-decorator";
+import {
+  BNavbar,
+  BNavbarBrand,
+  BNavbarToggle,
+  BCollapse,
+  BNavbarNav,
+  BNavItem,
+} from "bootstrap-vue";
 
 @Component({
   components: {
@@ -36,18 +63,18 @@ export default class Navbar extends Vue {
   showY: number = 100;
 
   created() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener("scroll", this.handleScroll);
   }
 
   destroyed() {
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener("scroll", this.handleScroll);
   }
 
   changePage(page: string) {
-    this.$emit('changePage', page);
+    this.$emit("changePage", page);
   }
 
-  handleScroll (event: Event) {
+  handleScroll(event: Event) {
     console.log("Handling scroll event");
 
     let scrollTop = window.scrollY;
@@ -59,16 +86,17 @@ export default class Navbar extends Vue {
       this.isScrollingDown = true;
     }
     this.lastScrollTop = scrollTop;
-
   }
-};
+}
 </script>
 
 <style scoped>
 .scrolled-down {
-  transform:translateY(-100%); transition: all 0.5s ease-in-out;
+  transform: translateY(-100%);
+  transition: all 0.5s ease-in-out;
 }
 .scrolled-up {
-  transform:translateY(0); transition: all 0.5s ease-in-out;
+  transform: translateY(0);
+  transition: all 0.5s ease-in-out;
 }
 </style>
